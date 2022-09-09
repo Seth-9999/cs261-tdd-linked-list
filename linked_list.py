3be9d7ba78e2d224e69ea8300b5c8dfa17ab6714
Seth <jonRiemer.88@outlook.com>
2022-09-08 21:12:09 -0400

    Added my name (first commit!)

--- linked_list.py
+++ linked_list.py
@@ -5,7 +5,7 @@
 # If you can't figure it out recursively, use a loop. (But then refactor
 # your implementation into a recursive one!)
 # Your implementation should pass the tests in test_sorted_list.py.
-# YOUR NAME
+# Jon Riemer
 
 class LinkedList:
 
